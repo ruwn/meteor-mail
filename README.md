49a470482c81d5f5a988f06a2785472dad28a09f
Rouven Dreimann <rdreimann@fh-bielefeld.de>
2015-06-30 15:58:07 +0200

    folder werden angezeigt

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ## Mongo dump einpflegen
 mongorestore -h 127.0.0.1 --port 3001 -d meteor db-dump/
+
+## Distinct
+https://libraries.io/meteor/mrt:mongodb-aggregation
+
+meteor add mrt:mongodb-aggregation
--- meteor-mail.js
+++ meteor-mail.js
@@ -4,17 +4,22 @@ if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
         folders: function () {
-            return Mails.find({});
+            var data = Mails.find().fetch();
+            var distinctData = _.uniq(data, false, function(d) {return d.folder});
+            return _.pluck(distinctData, "folder");
         }
     });
 
-    Template.task.events({
+    Template.folder.events({
         "click .toggle-checked": function () {
             // Set the checked property to the opposite of its current value
             Tasks.update(this._id, {$set: {checked: !this.checked}});
         },
-        "click .delete": function () {
-            Mails.remove(this._id);
+        "click .delete": function (e) {
+            var folderName = event.target.value;
+            console.log(".delete");
+            console.log(folderName);
+
         }
     });
 }
