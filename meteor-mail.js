ce61626e33e81cd3bd0e8c2bacfc67eb6403e97a
Rouven Dreimann <rdreimann@fh-bielefeld.de>
2015-06-30 14:16:11 +0200

    rumgespielt...

--- meteor-mail.js
+++ meteor-mail.js
@@ -1,40 +1,20 @@
-
 Mails = new Mongo.Collection("mails");
 
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
-      // Show newest tasks first
-      return Mails.find({});
-    }
-  });
+    // This code only runs on the client
+    Template.body.helpers({
+        folders: function () {
+            return Mails.find({});
+        }
+    });
 
-  Template.body.events({
-    "submit .new-task": function (event) {
-      // This function is called when the new task form is submitted
-      var text = event.target.text.value;
-
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
-
-      // Clear form
-      event.target.text.value = "";
-
-      // Prevent default form submit
-      return false;
-    }
-  });
-
-  Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
-    },
-    "click .delete": function () {
-      Tasks.remove(this._id);
-    }
-  });
+    Template.task.events({
+        "click .toggle-checked": function () {
+            // Set the checked property to the opposite of its current value
+            Tasks.update(this._id, {$set: {checked: !this.checked}});
+        },
+        "click .delete": function () {
+            Mails.remove(this._id);
+        }
+    });
 }
